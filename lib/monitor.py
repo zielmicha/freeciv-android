4ba2fa43aba850ea0259fc0a768519f83fb22bb6
Michał Zieliński <michal@zielinscy.org.pl>
2012-01-11 21:33:07 +0100

    added license headers

--- lib/monitor.py
+++ lib/monitor.py
@@ -1,3 +1,15 @@
+# Copyright (C) 2011 Michal Zielinski (michal@zielinscy.org.pl)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2, or (at your option)
+# any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
 import thread
 import save
 import os
