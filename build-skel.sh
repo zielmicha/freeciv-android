f35caa424c362c38dee9a19ffc3cb4c2d626e367
Michał Zieliński <michal@zielinscy.org.pl>
2013-03-03 12:44:35 +0100

    fix build scripts

--- build-skel.sh
+++ build-skel.sh
@@ -1,7 +1,4 @@
-ln -sf $PWD/src/utility server
-ln -sf $PWD/src/common server
 mkdir src/objectfiles
-mkdir server/objectfiles
 mkdir android/build/apk
 
 PYGAME_SUBSET=android/build/pygame/libs/armeabi
--- build.sh
+++ build.sh
@@ -1,9 +1,6 @@
 echo Configuring...
 ./download-dep || exit
 ./build-skel.sh || exit
-cd server/
-./configure || exit
-cd ..
 cd src/
 ./configure || exit
 cd ..
