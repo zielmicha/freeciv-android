109db6552656f25959e5a53306537ca25a4ec0d0
Michał Zieliński <michal@zielinscy.org.pl>
2013-04-05 16:27:09 +0200

    script for building release

---
+++ android/release.mk
@@ -0,0 +1,33 @@
+release: build/build.xml debug ndk
+	cd build; ant release
+
+debug:
+	$(MAKE) -f Makefile
+
+build:
+	mkdir build
+
+build/AndroidManifest.xml: build project/AndroidManifest.xml
+	cat project/AndroidManifest.xml \
+		| sed 's/pl.org.zielinscy.freeciv.debug/pl.org.zielinscy.freeciv/g' \
+		| sed 's/Freeciv Debug/Freeciv/g' \
+		| cat  >  build/AndroidManifest.xml
+
+build/build.xml: build/AndroidManifest.xml _links
+	android update project --path build --name "Freeciv" --target android-17
+
+ndk:
+	ndk-build -C build
+
+_links: build build/src build/jni build/res build/obj build/assets
+
+build/src:
+	ln -sf $(PWD)/project/src build/src
+build/jni:
+	ln -sf $(PWD)/project/jni build/jni
+build/res:
+	ln -sf $(PWD)/project/res build/res
+build/obj:
+	ln -sf $(PWD)/project/obj build/obj
+build/assets:
+	ln -sf $(PWD)/project/assets build/assets
