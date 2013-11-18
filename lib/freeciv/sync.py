682eb8845b3feafe88bbe75a0eede6c52dea4bf8
Michał Zieliński <michal@zielinscy.org.pl>
2013-11-18 21:05:42 +0100

    civsync: different user agent for desktop

--- lib/freeciv/sync.py
+++ lib/freeciv/sync.py
@@ -46,7 +46,12 @@ def apply_user_agent_change(ua):
     urllib.URLopener.version = ua
 
 features.set_applier('civsync.ua', apply_user_agent_change)
-features.set('civsync.ua', 'CivSyncAndroid/1108')
+
+version = 1108
+if osutil.is_desktop:
+    features.set('civsync.ua', 'CivSyncDesktop/%d' % version)
+else:
+    features.set('civsync.ua', 'CivSyncAndroid/%d' % version)
 features.add_feature('civsync.enable', False, type=bool)
 features.add_feature('civsync.sid', None)
 
