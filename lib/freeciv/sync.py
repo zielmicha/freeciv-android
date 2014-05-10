5de9c897e48b42cf580edc05af10f8441ab6b348
Michal Zielinski <michal@zielinscy.org.pl>
2014-05-10 18:36:25 +0200

    bump version to 1.1.10

--- lib/freeciv/sync.py
+++ lib/freeciv/sync.py
@@ -47,7 +47,7 @@ def apply_user_agent_change(ua):
 
 features.set_applier('civsync.ua', apply_user_agent_change)
 
-version = 1109
+version = 1110
 if osutil.is_desktop:
     features.set('civsync.ua', 'CivSyncDesktop/%d' % version)
 else:
