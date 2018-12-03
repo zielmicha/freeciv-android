3ba6ed6b837106b7823fd50ca76b65cb6f7a5f61
Pierre Renié <javax.ubuntu@gmail.com>
2018-12-03 23:15:30 +0100

    Request storage permission if it's not enabled

--- android/project/src/main/java/org/libsdl/app/SDLActivity.java
+++ android/project/src/main/java/org/libsdl/app/SDLActivity.java
@@ -29,6 +29,10 @@ import android.graphics.drawable.Drawable;
 import android.media.*;
 import android.hardware.*;
 import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.app.ActivityCompat;
 
 /**
     SDL Activity
@@ -161,6 +165,36 @@ public class SDLActivity extends Activity {
            return;
         }
 
+		if (!haveStoragePermissions()) {
+			String[] requiredPermissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE};
+			ActivityCompat.requestPermissions(this, requiredPermissions, 1);
+			synchronized(this) {
+				try {
+					wait();
+				} catch (InterruptedException ex) {
+					ex.printStackTrace();
+				}
+			}
+
+			if (!haveStoragePermissions()) {
+				AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(this);
+		        dlgAlert.setMessage("Freeciv cannot start without access to storage.");
+		        dlgAlert.setTitle("Storage access required");
+		        dlgAlert.setPositiveButton("Exit",
+		            new DialogInterface.OnClickListener() {
+		                @Override
+		                public void onClick(DialogInterface dialog,int id) {
+		                    // if this button is clicked, close current activity
+		                    SDLActivity.mSingleton.finish();
+		                }
+		            });
+		       dlgAlert.setCancelable(false);
+		       dlgAlert.create().show();
+
+		       return;
+			}
+		}
+
         // Set up the surface
         mSurface = new SDLSurface(getApplication());
 
@@ -187,6 +221,17 @@ public class SDLActivity extends Activity {
             }
         }
     }
+
+	@Override
+	public synchronized void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+		notify();
+	}
+
+	private boolean haveStoragePermissions() {
+		return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+			&& ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
+	}
+
 
     // Events
     @Override
